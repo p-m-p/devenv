18af7c403b3665f13689afbd988d1d24197c15ef
vagrant <vagrant@development-precise64.(none)>
2013-03-26 11:50:33 +0000

    Add install for vim

--- install.sh
+++ install.sh
@@ -1,14 +1,36 @@
 #!/usr/bin/env bash
 
-# config scripts
+# Copy config scripts
 config_files="vimrc tmux.conf"
 for config in $config_files
 do
-  cp ./config "~/.$(config)"
+  cp ./$config ~/.$config
 done
 
 # Setup vim and plugins
-cp -r vim ~/.vim
-git clone git@github.com:tpope/vim-haml.git ~/.vim/bundle/vim-haml
-git clone git@github.com:nono/vim-handlebars.git ~/.vim/bundle/vim-handlebars
-git clone git@github.com:pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript
+vim_dirs="autoload bundle colors"
+for vim_dir in $vim_dirs
+do
+  if [ ! -d ~/.vim/$vim_dir ]; then
+    mkdir ~/.vim/$vim_dir
+  fi
+
+  if [ -d ./vim/$vim_dir ]; then
+    for vim_file in $( ls ./vim/$vim_dir )
+    do
+      cp ./vim/$vim_dir/$vim_file ~/.vim/$vim_dir/$vim_file
+    done
+  fi
+done
+
+declare -A vim_plugins
+vim_plugins["vim-haml"]="git@github.com:tpope/vim-haml.git"
+vim_plugins["vim-handlebars"]="git@github.com:nono/vim-handlebars.git"
+vim_plugins["vim-javascript"]="git@github.com:pangloss/vim-javascript.git"
+
+for plugin in ${!vim_plugins[@]}
+do
+  if [ ! -d ~/.vim/bundle/$plugin ]; then
+    git clone ${vim_plugins[$plugin]} ~/.vim/bundle/$plugin
+  fi
+done
