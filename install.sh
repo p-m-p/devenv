fceb9ce5e10f6b58db1c99d744c014964b8d0bf5
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-25 21:48:14 +0100

    Update for Neovim config installation

--- install.sh
+++ install.sh
@@ -1,62 +1,21 @@
 #!/usr/bin/env bash
 
+set -e
+
 [ -z "$HOME_DIR" ] && HOME_DIR=~
-VIM_DIR="$HOME_DIR/.vim"
 
-vim_plugins=(
-  "https://github.com/kien/ctrlp.vim.git"
-  "https://github.com/pangloss/vim-javascript.git"
-  "https://github.com/tpope/vim-fugitive.git"
-  "https://github.com/bling/vim-airline.git"
-  "https://github.com/vim-airline/vim-airline-themes.git"
-  "https://github.com/vim-ruby/vim-ruby.git"
-  "https://github.com/plasticboy/vim-markdown.git"
-  "https://github.com/leafgarland/typescript-vim.git"
-)
-
-# Copy config scripts
 for config in $( ls ./dot-files )
 do
+  echo "Installing .$config"
   cp ./dot-files/$config $HOME_DIR/.$config
 done
 
-# TMUX session start up
-if [ -d ~/development ]; then
-  cp ./tmux-launch $HOME_DIR/development
-else
-  cp ./tmux-launch $HOME_DIR
+if [ ! -d "$HOME_DIR/.tmux-themepack" ]; then 
+  echo "Installing TMUX theme at $HOME_DIR/.tmux-themepack"
+  git clone --depth=1 https://github.com/jimeh/tmux-themepack.git $HOME_DIR/.tmux-themepack
 fi
 
-if [ ! -d $VIM_DIR ]; then
-  echo 'Unable to locate .vim directory for current user'
-  exit 1
+if [ ! -d "$HOME_DIR/.config/nvim" ]; then
+  echo "Installing Neovim configs"
+  git clone https://github.com/p-m-p/nvim-config.git $HOME_DIR/.config/nvim
 fi
-
-# Setup vim
-for fldr in $( ls ./vim )
-do
-  if [ ! -d $VIM_DIR/$fldr ]; then
-    echo "Setting up vim directory: $fldr"
-    mkdir $VIM_DIR/$fldr
-  fi
-
-  if [ -d ./vim/$fldr ]; then
-    for vim_file in $( ls ./vim/$fldr )
-    do
-      cp -r ./vim/$fldr/$vim_file $VIM_DIR/$fldr/$vim_file
-    done
-  fi
-done
-
-# Install Vim plugins
-for plugin in ${vim_plugins[@]}
-do
-  plugin_name=$( echo $plugin | sed -E 's;^.*/(.*).git$;\1;' )
-  plugin_dir=$VIM_DIR/bundle/$plugin_name
-
-  if [ ! -d $plugin_dir ]; then
-    echo
-    echo "Installing vim plugin: $plugin_name"
-    git clone $plugin $plugin_dir
-  fi
-done
