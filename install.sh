0e99bf7f5c58e47eb2ef87855d5a56e8c5cb240d
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-26 21:19:23 +0100

    Add zsh and czrc files with oh my zsh

--- install.sh
+++ install.sh
@@ -11,11 +11,16 @@ do
 done
 
 if [ ! -d "$HOME_DIR/.tmux-themepack" ]; then 
-  echo "Installing TMUX theme at $HOME_DIR/.tmux-themepack"
+  echo "Installing TMUX theme"
   git clone --depth=1 https://github.com/jimeh/tmux-themepack.git $HOME_DIR/.tmux-themepack
 fi
 
 if [ ! -d "$HOME_DIR/.config/nvim" ]; then
   echo "Installing Neovim configs"
   git clone https://github.com/p-m-p/nvim-config.git $HOME_DIR/.config/nvim
+fi
+
+if [ ! -d "$HOME_DIR/.oh-my-zsh" ]; then 
+  echo "Installing Oh My Zsh"
+  git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git $HOME_DIR/.oh-my-zsh
 fi
