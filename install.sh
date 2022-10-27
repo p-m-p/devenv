c222203f95c35842cff7b1dc5bd7d3994fc59e12
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-27 21:34:12 +0100

    Add node and nvm install

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ do
   cp ./dot-files/$config $HOME_DIR/.$config
 done
 
-if [ ! -d "$HOME_DIR/.tmux-themepack" ]; then 
+if [ ! -d "$HOME_DIR/.tmux-themepack" ]; then
   echo "Installing TMUX theme"
   git clone --depth=1 https://github.com/jimeh/tmux-themepack.git $HOME_DIR/.tmux-themepack
 fi
@@ -20,7 +20,14 @@ if [ ! -d "$HOME_DIR/.config/nvim" ]; then
   git clone https://github.com/p-m-p/nvim-config.git $HOME_DIR/.config/nvim
 fi
 
-if [ ! -d "$HOME_DIR/.oh-my-zsh" ]; then 
+if [ ! -d "$HOME_DIR/.oh-my-zsh" ]; then
   echo "Installing Oh My Zsh"
   git clone --depth=1 https://github.com/ohmyzsh/ohmyzsh.git $HOME_DIR/.oh-my-zsh
+fi
+
+if [ ! -d "$HOME_DIR/.nvm" ]; then
+  echo "Installing Node with nvm"
+  git clone https://github.com/nvm-sh/nvm.git $HOME_DIR/.nvm
+  zsh -c "source $HOME_DIR/.nvm/nvm.sh && nvm install --lts"
+  echo "lts/*" > $HOME_DIR/.nvmrc
 fi
