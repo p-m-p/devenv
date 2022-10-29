4b9acc696dc678aea7f04bdcc51bd630455b5dc1
Phil Parsons <p-m-p@users.noreply.github.com>
2022-10-29 12:27:55 +0100

    Better messages and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+Config file setup
+===
+
+Required Software
+
+- [ZSH](https://www.zsh.org/)
+- [Starship](https://starship.rs/)
+- [tmux](https://github.com/tmux/tmux/wiki)
+- [Neovim](https://neovim.io/)
+
+Run `install.sh` to copy dot files and install plugins.
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 set -e
 
@@ -6,8 +6,8 @@ set -e
 
 for config in $( ls ./dot-files )
 do
-  echo "Installing .$config"
   cp ./dot-files/$config $HOME_DIR/.$config
+  echo "Added $HOME_DIR/.$config"
 done
 
 if [ ! -d "$HOME_DIR/.tmux" ]; then
@@ -16,14 +16,13 @@ if [ ! -d "$HOME_DIR/.tmux" ]; then
 fi
 
 if [ ! -d "$HOME_DIR/.config/nvim" ]; then
-  echo "Installing Neovim configs and bootstrapping"
+  echo "Installing Neovim configs"
   git clone https://github.com/p-m-p/nvim-config.git $HOME_DIR/.config/nvim
-  nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 fi
 
 if [ ! -d "$HOME_DIR/.nvm" ]; then
-  echo "Installing Node with nvm"
+  echo "Installing nvm"
   git clone https://github.com/nvm-sh/nvm.git $HOME_DIR/.nvm
-  zsh -c "source $HOME_DIR/.nvm/nvm.sh && nvm install --lts"
+  source $HOME_DIR/.nvm/nvm.sh && nvm install --lts
   echo "lts/*" > $HOME_DIR/.nvmrc
 fi
