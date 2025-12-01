7008c75a4e1ee3cf3c3e9e6ef1a5c0a1e2477b0d
Phil Parsons <p-m-p@users.noreply.github.com>
2025-12-01 11:38:10 +0000

    feat: install vim plug

--- install.sh
+++ install.sh
@@ -52,6 +52,13 @@ if [ ! -d "$TPM_DIR" ]; then
   git clone https://github.com/tmux-plugins/tpm "$TPM_DIR"
 fi
 
+VIM_PLUG_FILE="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
+if [ ! -f "$VIM_PLUG_FILE" ]; then
+  echo "📦 Installing vim-plug"
+  curl -fLo "$VIM_PLUG_FILE" --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
 if [ ! -d "$NVIM_CONFIG_DIR" ]; then
   git clone https://github.com/p-m-p/nvim-config.git "$NVIM_CONFIG_DIR"
